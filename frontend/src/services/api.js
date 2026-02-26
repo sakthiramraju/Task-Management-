868f74da1444d9385410868c43b6671108c95f2c
Sakthiram-R <sakthivani002@gmail.com>
2026-02-26 18:50:38 +0530

    fix: in api and env

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 API.interceptors.request.use(config => {
   const token = localStorage.getItem("access");
   if (token) {
-    config.headers["Authorization"] = `Bearer ${token}`;
+    config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
 });
